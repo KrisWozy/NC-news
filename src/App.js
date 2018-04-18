35b6a8d85d0e7dffc54089f924b92571ba9fa64f
Kris Wozencroft <kriswozy@Kriss-MacBook-Pro.local>
2018-04-18 14:34:39 +0100

    initial set-up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>NC NEWS</h1>
+        <h1>Topics</h1>
+        <h1>AricleList</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
